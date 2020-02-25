aad380195dff9ba5a1bae2cd44e5eccc5eb50eca
Madeleine Zhu <yz3488@york.ac.uk>
2020-02-25 09:54:29 +0000

    tutorial 20.02.25

--- Tutorial 1.R
+++ Tutorial 1.R
@@ -1,7 +1,7 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-l
+
 
 #example: long titles and names as calculation going down
 sample <- rnom(100, mean = 100)
@@ -58,11 +58,35 @@ mod_2  <- update(mod, .~. -GSH:genotype)
 summary(mod_2)
 
 res <- anova(mod_2)
-res$Df[1] res$df[3]
-
+res$Df[1]
+res$df[3]
+summary(res)
 # Input effects of sensityvity/genotype
 
 #F value = mean (GSH or genotype Mean Sq)/ Mean Sq Residuals (dgree of freedom)
 #F value is the dgrees of freedom of p value
 #Prove the relationship shown is real
 
+#tutorial 02.25: making public graph
+plot <- ggplot(data = response,
+       aes(x = GSH, y = sens, col = genotype)) +
+  scale_colour_manual(values = viridis::viridis(3), name = "Genotype")+
+  geom_point() +
+  theme_classic()+
+  geom_smooth(method = "lm", se =FALSE)+
+  ylab("treatment sensitivity")+
+  theme_classic()+
+  theme(legend.position = c(0.2, 0.3), 
+        axis.title = element_text(size = 12),
+        axis.text = element_text(size = 10),
+        legend.text = element_text(size = 10),
+        legend.spacing.y = unit(0.05,"cm"))+
+  scale_y_continuous(limits = c(0,35), expand = c(0,0))+
+  scale_x_continuous(limits = c(0,7), expand = c(0,0))
+  
+install.packages("viridis")#viridis pallate
+
+#error in R markdown: lm: F= `r res$F[2]`; d.f. = `r$Df[1]`; d.f =`r res$Df[4]`;
+#p = `r res$Pr[1]`
+#d.f. = `r res$Df [1]`; `r res$df[3]`
+#The **mean** *distance* is `r m` and it is big. $p = \chi{2} and one half\ alp \frac{1}2$
